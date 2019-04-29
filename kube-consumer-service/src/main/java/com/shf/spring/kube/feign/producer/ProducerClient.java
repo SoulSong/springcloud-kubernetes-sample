29113a436e1b29b6ce327433ab733bc689299307
songhaifeng <songhaifengshuaige@126.com>
2019-04-30 01:28:27 +0800

    Remove unused import.

--- kube-consumer-service/src/main/java/com/shf/spring/kube/feign/producer/ProducerClient.java
+++ kube-consumer-service/src/main/java/com/shf/spring/kube/feign/producer/ProducerClient.java
@@ -3,7 +3,6 @@ package com.shf.spring.kube.feign.producer;
 import com.shf.spring.kube.configuration.FeignHeaderInterceptor;
 import com.shf.spring.kube.producer.service.HelloService;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.context.annotation.Profile;
 
 /**
  * @author songhaifeng
