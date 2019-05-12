2b77127fdc306301a9abd7c975d389da89e70202
songhaifeng <songhaifeng@patsnap.com>
2019-05-12 19:37:31 +0800

    Add configmap & secret for consumer-service.

--- kube-consumer-service/src/main/java/com/shf/spring/kube/configuration/BeanConfiguration.java
+++ kube-consumer-service/src/main/java/com/shf/spring/kube/configuration/BeanConfiguration.java
@@ -1,5 +1,9 @@
 package com.shf.spring.kube.configuration;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /**
@@ -9,5 +13,19 @@ import org.springframework.context.annotation.Configuration;
  * @Date: 2019/4/15 13:06
  */
 @Configuration
+@EnableConfigurationProperties
 public class BeanConfiguration {
+
+    @Bean
+    @ConfigurationProperties(prefix = "configs")
+    public Variables variables(){
+        return new Variables();
+    }
+
+
+    @Data
+    public class Variables{
+        private String variable1;
+        private String variable2;
+    }
 }
--- kube-consumer-service/src/main/java/com/shf/spring/kube/feign/producer/ProducerClient.java
+++ kube-consumer-service/src/main/java/com/shf/spring/kube/feign/producer/ProducerClient.java
@@ -2,10 +2,12 @@ package com.shf.spring.kube.feign.producer;
 
 import com.shf.spring.kube.feign.FeignHeaderInterceptor;
 import com.shf.spring.kube.producer.endpoint.HelloEndpoint;
+import org.springframework.cloud.kubernetes.ribbon.KubernetesServerList;
 import org.springframework.cloud.openfeign.FeignClient;
 
 /**
- * producer-service client binder
+ * producer-service client binder, discovery serves by ribbon.
+ * {@link KubernetesServerList}
  *
  * @author songhaifeng
  */
