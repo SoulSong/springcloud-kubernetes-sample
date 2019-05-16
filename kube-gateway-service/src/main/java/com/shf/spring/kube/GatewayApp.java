1f0bf7b775692721e87d30ddd210a523f85e4ab5
songhaifeng <songhaifengshuaige@126.com>
2019-05-17 00:36:57 +0800

    Add a distributed rate limiter by redis, use the 'Token Bucket Algorithm'.

--- kube-gateway-service/src/main/java/com/shf/spring/kube/GatewayApp.java
+++ kube-gateway-service/src/main/java/com/shf/spring/kube/GatewayApp.java
@@ -2,6 +2,12 @@ package com.shf.spring.kube;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import reactor.core.publisher.Mono;
+
+import java.util.Collections;
 
 /**
  * Gateway app
@@ -9,8 +15,19 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author songhaifeng
  */
 @SpringBootApplication
+@Configuration
 public class GatewayApp {
     public static void main(String[] args) {
         SpringApplication.run(GatewayApp.class, args);
+    }
+
+    /**
+     * Get userId from the request header as limit key
+     *
+     * @return
+     */
+    @Bean
+    KeyResolver userKeyResolver() {
+        return exchange -> Mono.just(exchange.getRequest().getHeaders().getOrDefault("userId", Collections.singletonList("anonymity")).get(0));
     }
 }
